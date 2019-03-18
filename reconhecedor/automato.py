74faa928aeb45f8d058c723ca01c351d75b17d4c
Daniel Matheus Brandão Lent <danbranlent@yahoo.com.br>
2019-03-18 19:23:22 -0300

    Iniciando classe automato

--- reconhecedor/automato.py
+++ reconhecedor/automato.py
@@ -1,6 +1,45 @@
 class Reconhecedor:
 
-    def __init__(self):
+    def __init__(self, states:dict, startState:Estado, finalStates:dict, transitions:dict):
+        self._states: dict = states
+        self._finalStates: dict = finalStates
+        self._startState: Estado = startState
+
         pass
     
+
+    def automata():
+        pass
     
+    def setState(id:int):
+        pass
+
+    def setFinalState(id:int):
+        pass
+
+    def setTransition(origin:int, destiny:int, symbol:str):
+        pass
+    
+    def getTransition(origin:int, symbol:str) -> Transicao:
+        pass
+    
+    def getStartState() -> Estado:
+        pass
+
+    def getFinalState(id:int) -> bool:
+        pass
+    
+    def getState(id:int) -> bool:
+        pass
+    
+    def isStartState(id:int) -> bool:
+        pass
+
+    def isFinalState(id:int) -> bool:
+        pass
+    
+    def getFinalStateSize():
+        pass
+    
+    def message():
+        pass
