197dc91770888ed42c6db94d6697f604711ddf73
Daniel Matheus Brandão Lent <danbranlent@yahoo.com.br>
2019-03-19 10:39:05 -0300

    Autômato reconhecedor adicionado à main.py

--- main.py
+++ main.py
@@ -5,36 +5,61 @@ if __name__ == '__main__':
     automata = Automato()
 
     # Setando os estados
-    for i in range(8):
+    for i in range(1, 15):
         automata.setState(i)
 
     # Estados finais e inicial
-    automata.setStartState(0)
+    automata.setStartState(1)
 
     automata.setFinalState(2)
+    automata.setFinalState(3)
     automata.setFinalState(4)
+    automata.setFinalState(5)
     automata.setFinalState(6)
     automata.setFinalState(7)
+    automata.setFinalState(8)
+    automata.setFinalState(9)
+    automata.setFinalState(10)
+    automata.setFinalState(11)
+    automata.setFinalState(12)
+    automata.setFinalState(13)
+    automata.setFinalState(14)
 
     # Transicoes
-    automata.setTransition(0, 0, '0')
-    automata.setTransition(1, 3, '0')
-    automata.setTransition(2, 0, '0')
-    automata.setTransition(3, 2, '0')
-    automata.setTransition(4, 3, '0')
-    automata.setTransition(5, 6, '0')
-    automata.setTransition(6, 2, '0')
-    automata.setTransition(7, 6, '0')
+    automata.setTransition(1, 2, '[a-z]')
+    automata.setTransition(2, 2, '[a-z0-9]')
 
-    automata.setTransition(0, 1, '1')
-    automata.setTransition(1, 5, '1')
-    automata.setTransition(2, 1, '1')
-    automata.setTransition(3, 4, '1')
-    automata.setTransition(4, 5, '1')
-    automata.setTransition(5, 7, '1')
-    automata.setTransition(6, 4, '1')
-    automata.setTransition(7, 7, '1')
+    automata.setTransition(1, 3, ' \\n')
+    automata.setTransition(3, 3, ' \\n')
 
-    entrada = '11101100100'
+    automata.setTransition(1, 4, '[0-9]')
+    automata.setTransition(4, 4, '[0-9]')
+    automata.setTransition(4, 5, '\.')
+    automata.setTransition(5, 5, '[0-9]')
+
+    automata.setTransition(1, 6, '[\',;\)=/\[\]\{\}_]')
+
+    automata.setTransition(1, 7, ':')
+    automata.setTransition(7, 8, '=')
+
+    automata.setTransition(1, 9, '\.')
+    automata.setTransition(9, 8, '\.')
+
+    automata.setTransition(1, 10, '<')
+    automata.setTransition(10, 8, '[=>]')
+
+    automata.setTransition(1, 11, '>')
+    automata.setTransition(11, 8, '=')
+
+    automata.setTransition(1, 12, '\(')
+    automata.setTransition(12, 8, '\*')
+
+    automata.setTransition(1, 13, '\*')
+    automata.setTransition(13, 8, '\)')
+
+    automata.setTransition(1, 14, '[+-]')
+    automata.setTransition(14, 4, '[0-9]')
+
+    entrada = '-1'
 
     print(automata.test(entrada))
