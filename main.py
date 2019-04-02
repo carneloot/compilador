60df673bc6d96ac2eac7354a295d0bbcfae60a24
Vinicius Emanuel Cavalheri Verdade <vinicius-verdade@hotmail.com>
2019-04-02 14:35:17 -0300

    Corrigindo erros da analise lexica

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from reconhecedor.automato import Automato
+from reconhecedor.lexico import *
 
 if __name__ == '__main__':
 
     automato = Automato.fromFile('./automato.aut')
 
     automato.enableLogging()
+    boolr, m = analiseLexica(automato, 'nome 2 3.a 123415')
+    print(m)
 
-    print(automato.test('3A'))
--- reconhecedor/lexico.py
+++ reconhecedor/lexico.py
@@ -1,24 +1,31 @@
-from ../reconhecedor.automato import Automato
+from .automato import Automato
 
 
 def analiseLexica(automato: Automato, entrada: str):
+    entrada += ' '
     coluna_atual = 0
     vetor_de_tokens = []
     vetor_de_tokens.append('inicio')
     tamanho_entrada = len(entrada)
     label_anterior = 'inicio'
-    for i in enumerate(entrada):
+    while len(entrada) > 1:
         saida, label, posicao = automato.test(entrada)
 
         # Se o automato retornar falso, um erro foi encontrado
         # Parar a análise e avisar o problema
         if saida is False:
+            print(vetor_de_tokens)
             return (False, f'Erro na coluna {coluna_atual + posicao} depois do token:\'{vetor_de_tokens[-1]}\'')
         else:
-            if (label_anterior is 'NUM') or (label_anterior is 'REAL')
+            if (label_anterior == 'Inteiro') or (label_anterior == 'Real'):
+                if (label != 'Espaco' and label != 'Simbolo especial' and label != 'Simbolo Composto'):
+                    vetor_de_tokens.pop()
+                    print(vetor_de_tokens)
+                    return (False, f'Erro na coluna {coluna_atual + posicao} depois do token:\'{vetor_de_tokens[-1]}\'')
 
             token = entrada[:posicao]
             entrada = entrada[posicao:]
+            print(f'Token "{token}" entrada "{entrada}"')
             vetor_de_tokens.append(token)
             # fim do token
         label_anterior = label
