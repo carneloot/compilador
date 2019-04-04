107ea93ce196e891ef8d4b7e5451b8e9ff38dc62
Ragnaranger <danbranlent@yahoo.com.br>
2019-04-04 17:23:14 -0300

    Criando variável de palavras reservadas na main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from reconhecedor.automato import Automato
+from palavras_reservadas import *
 
 if __name__ == '__main__':
 
     automato = Automato.fromFile('./automato.aut')
 
     automato.enableLogging()
+
+    palavras_reservadas = criar_palavras_reservadas("palavras_reservadas.txt")
 
     print(automato.test('3.141592653589'))
