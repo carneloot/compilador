bfe7567a3de1778d37998c1c7c18c3edbd59d6fe
Matheus Carnelutt <matheuschafrao@gmail.com>
2019-05-14 12:43:16 -0300

    Adicionando print da tabela hash no terminal

--- main.py
+++ main.py
@@ -47,6 +47,9 @@ if __name__ == '__main__':
         # Printa a tabela hash no arquivo
         with open(f'{argumentos.saida}_tabela.txt', 'w') as arquivo:
             tabela_ids.print(arquivo, print_none=not argumentos.skip_empty)
+        
+        # Printa a tabela hash no terminal
+        tabela_ids.print(print_none=not argumentos.skip_empty)
 
         # Printa os tokens
         with open(f'{argumentos.saida}_tokens.txt', 'w') as arquivo:
